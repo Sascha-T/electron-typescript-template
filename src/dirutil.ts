91429b63ec9efbf88bfe48ef781908047c59a601
Sascha_T <info@sascha-t.de>
2019-11-06 22:11:56 +0100

    Revert fuckup
    This reverts commit 376eda4b601beec732433bd6085bde7bfe9d4b47.

--- src/dirutil.ts
+++
@@ -1,8 +0,0 @@
-import * as mkdirp from "mkdirp";
-export async function smkdirp(path) {
-    return new Promise(async (res) => {
-        mkdirp(path, () => {
-            res();
-        })
-    });
-}
